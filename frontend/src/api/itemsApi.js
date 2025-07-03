6322b062198216f354dcbf37f0f6287bbc69b653
nesteagle <104323706+nesteagle@users.noreply.github.com>
2025-07-02 21:47:27 -0700

    Implemented get item by id in frontend and removed unused CSS

--- frontend/src/api/itemsApi.js
+++ frontend/src/api/itemsApi.js
@@ -8,3 +8,13 @@ export async function getItems(query = null) {
     const response = await apiRequest(endpoint, "GET");
     return response.items;
 }
+
+export async function getItem(id) {
+    const int_id = parseInt(id);
+    if (int_id == null || !Number.isInteger(int_id) || int_id <= 0) {
+        throw new Error("Valid positive integer ID required");
+    }
+    const endpoint = `/items/${int_id}`
+    const response = await apiRequest(endpoint, "GET");
+    return response.item;
+}
